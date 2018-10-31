d648882fd6eb2b2faef3f2fa51422d54f9c4ed4e
Sebastian Civarolo <scivarolo@gmail.com>
2018-10-31 00:06:52 -0500

    Add API object with post method

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,16 +1,20 @@
-const legoCreation = {
-  id: 1,
-  creator: "Per Sun",
-  color: "Black",
-  type: "Animal",
-  creation: "Black Cow"
+const API = {
+  url: "http://localhost:8088/legos",
+  post(object) {
+    return fetch(this.url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(object)
+    })
+  }
 }
-
 document.querySelector(".lego__save").addEventListener("click", event => {
-  const creator = document.querySelector(".lego__creator").value
-  const creation = document.querySelector(".lego__creation").value
-  const color = document.querySelector("lego__color").value
-  const type = document.querySelector("lego__type").value
+  const creator = document.querySelector("#lego__creator").value
+  const creation = document.querySelector("#lego__creation").value
+  const color = document.querySelector("#lego__color").value
+  const type = document.querySelector("#lego__type").value
 
   const legoToSave = {
     creator: creator,
@@ -18,5 +22,5 @@ document.querySelector(".lego__save").addEventListener("click", event => {
     color: color,
     type: type
   }
+  API.post(legoToSave)
 })
-
